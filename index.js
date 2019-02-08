91157d659aa2d37d42a22c1bf219bc5f41a40971
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2019-02-08 12:13:36 +0000

    functions

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ function drawCircle(x, y, r) {
       "http://www.w3.org/2000/svg",
       "circle"
    );
-   circle.setAttributeNS(null, "cx", 100);
-   circle.setAttributeNS(null, "cy", 100);
-   circle.setAttributeNS(null, "r", 50);
+   circle.setAttributeNS(null, "cx", x);
+   circle.setAttributeNS(null, "cy", y);
+   circle.setAttributeNS(null, "r", r);
    circle.setAttributeNS(null, "fill", "yellow");
    circle.setAttributeNS(null, "stroke", "red");
    circle.setAttributeNS(null, "stroke-width", "1");
    document.getElementById("mySvg").appendChild(circle);
 }
-function drawRectangle(x, y, width, height) {
+function drawSquare(x, y, width) {
    let rectangle = document.createElementNS(
       "http://www.w3.org/2000/svg",
       "rect"
@@ -24,24 +24,14 @@ function drawRectangle(x, y, width, height) {
    rectangle.setAttributeNS(null, "x", x);
    rectangle.setAttributeNS(null, "y", y);
    rectangle.setAttributeNS(null, "width", width);
-   rectangle.setAttributeNS(null, "height", height);
+   rectangle.setAttributeNS(null, "height", width);
    rectangle.setAttributeNS(null, "fill", "cornflowerblue");
    rectangle.setAttributeNS(null, "stroke", "black");
    rectangle.setAttributeNS(null, "stroke-width", "1");
    document.getElementById("mySvg").appendChild(rectangle);
 }
-function drawPolygon(points) {
-   let polygon = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "polygon"
-   );
-   polygon.setAttributeNS(null, "points", "200,10 250,190 160,210");
-   polygon.setAttributeNS(null, "fill", "lime");
-   polygon.setAttributeNS(null, "stroke", "purple");
-   polygon.setAttributeNS(null, "stroke-width", "1");
-   document.getElementById("mySvg").appendChild(polygon);
-}
 
-drawPolygon();
-drawCircle(200, 200, 40);
-drawRectangle(100, 200, 80, 80);
+drawCircle(100, 100, 50);
+drawSquare(150, 50, 100);
+drawSquare(50, 150, 100);
+drawCircle(200, 200, 50);
